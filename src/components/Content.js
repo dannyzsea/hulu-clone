0c9e62a49e7473efbb901eab90797fd9940e7a52
John Doe <javatechisland@gmail.com>
2020-09-25 21:25:56 -0700

    api test action movies

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import VideoCard from "./VideoCard";
+import axios from "../axios";
+import requests from "../requests";
 import "./Content.css";
 function Content() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchActionMovies);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, []);
   return (
     <div className="content">
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
+      {movies.map((movie) => (
+        <VideoCard movie={movie} />
+      ))}
     </div>
   );
 }
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./VideoCard.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
 function VideoCard({ movie }) {
   return (
     <div className="videoCard">
-      <img src="https://static.toiimg.com/photo/72243079.jpeg" alt="" />
+      <img src={`${base_url}${movie.backdrop_path}`} alt="" />
       <p>This is a movie about an Orphan</p>
       <h2>Movie Title</h2>
       <p>Number of Likes!</p>
